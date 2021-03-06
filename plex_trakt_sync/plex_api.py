44bd5256dc17cbd337e8ef3bc0718f0b4a14296c
Elan Ruusamäe <glen@pld-linux.org>
2021-03-06 14:23:16 +0200

    Add PlexApi class abstraction

---
+++ plex_trakt_sync/plex_api.py
@@ -0,0 +1,15 @@
+from plex_trakt_sync.memoize import Memoize as memoize
+from plex_trakt_sync.requests_cache import CacheDisabledDecorator as nocache
+from plex_trakt_sync.main import get_plex_server
+
+
+class PlexApi:
+    """
+    Plex API class abstracting common data access and dealing with requests cache.
+    """
+
+    @property
+    @memoize
+    @nocache
+    def plex_server(self):
+        return get_plex_server()
