f0d80404ba60a41a98061c923d40b4728585d60a
Elan Ruusamäe <glen@pld-linux.org>
2021-03-06 13:57:10 +0200

    Add CacheDisabledDecorator to disable requests cache via decorator

--- plex_trakt_sync/requests_cache.py
+++ plex_trakt_sync/requests_cache.py
@@ -2,3 +2,12 @@ import requests_cache
 from .path import trakt_cache
 
 requests_cache.install_cache(trakt_cache)
+
+
+class CacheDisabledDecorator:
+    def __init__(self, fn):
+        self.fn = fn
+
+    def __call__(self, *args):
+        with requests_cache.disabled():
+            return self.fn(*args)
